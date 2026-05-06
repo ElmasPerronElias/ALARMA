6e3d9d6b90c4e82e23b1708e486a4c134d9bf7a1
elias filizzola <filizzola1245@gmail.com>
2026-05-05 20:50:21 -0500

    Actualización de configuración y corrección de URLs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://alarma-production.up.railway.app/api'
+  apiUrl: 'https://alarma-production.up.railway.app/api/dashboard/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://alarma-production.up.railway.app'
+  apiUrl: 'https://alarma-production.up.railway.app/api/dashboard/'
 };
